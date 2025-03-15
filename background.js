3e807c4557f64f9ee2f7a266c6f86adf2a766214
Berty Liu <bertylliu@gmail.com>
2025-03-15 11:10:12 +0800

    all merge

--- background.js
+++ background.js
@@ -1,4 +1,12 @@
+const urlLink = "https://json.extendsclass.com/bin/1684885865a7" // https://extendsclass.com/jsonstorage/1684885865a7
+const syncLink = "https://json.extendsclass.com/bin/d9a563320dff" //https://extendsclass.com/jsonstorage/d9a563320dff
+const vsLink = "https://json.extendsclass.com/bin/e16604375e31"//https://extendsclass.com/jsonstorage/e16604375e31
+let UID = false
+
+async function getTabs() {
+  return new Promise((resolve, ))
+}
+
 chrome.runtime.onInstalled.addListener(function() {
     console.log("擴展已安裝");
   });
-  
