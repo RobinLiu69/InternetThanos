f0191616257aed8d9355e4cc7e40222e6b070bd3
Anemois <danieltang1668@gmail.com>
2025-03-15 16:57:01 +0800

    added serverAddLinkData

--- background.js
+++ background.js
@@ -186,7 +186,7 @@ async function serverClearIdle(){
 	}
 }
 
-// import { serverAddLinkData } from "./tools.js"
+import { serverAddLinkData } from "./tools.js"
 
 function serverIsAdmin(){
 	return UID == "b0e03bdb-40b3-4950-8b12-170d80e90412"
