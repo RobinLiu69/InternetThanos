9c8cd8c028c805332cb957b1846a571ac4370499
Anemois <danieltang1668@gmail.com>
2025-03-15 11:35:26 +0800

    server.jsadd to manifest, server.js

--- background.js
+++ background.js
@@ -4,9 +4,11 @@ const vsLink = "https://json.extendsclass.com/bin/e16604375e31"//https://extends
 let UID = false
 
 async function getTabs() {
-  return new Promise((resolve, ))
+  return new Promise((resolve, reject) => {
+    
+  })
 }
-
+{id : "tabs", data: }
 chrome.runtime.onInstalled.addListener(function() {
     console.log("擴展已安裝");
   });
--- server.js
+++ server.js
@@ -3,12 +3,27 @@ const syncLink = "https://json.extendsclass.com/bin/d9a563320dff" //https://exte
 const vsLink = "https://json.extendsclass.com/bin/e16604375e31"//https://extendsclass.com/jsonstorage/e16604375e31
 let UID = false
 
-async function getTabs() {
-  return new Promise((resolve, reject) => {
-    
-  })
-}
-
 chrome.runtime.onInstalled.addListener(function() {
     console.log("擴展已安裝");
   });
+
+async function patchJSON(link, op){
+    fetch(link, {
+        method : "PATCH",
+        headers : {
+            "Content-type": "application/json-patch+json",
+        },
+        body : op
+    })
+}
+
+chrome.runtime.onMessage.addListener((message) => {
+    if(message.id == "tabs"){
+        data = message.data
+        uid = message.uid
+        for (let [url, uid] of data){
+            patchJSON(urlLink, JSON.stringify( [{ "op":"add", "path":"/"+[url]+"/"+[uid], "value":0 },
+                                                ] ))
+        }
+    }
+})
